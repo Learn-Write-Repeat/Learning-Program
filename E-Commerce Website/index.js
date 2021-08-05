338ea37c8231a50362af5c186c27c8f451189e0c
Md Adnan Hussain <hussainadnan906@gmail.com>
2021-08-05 16:43:33 +0530

    Modified all files, added account.html

--- E-Commerce Website/index.js
+++ E-Commerce Website/index.js
@@ -13,6 +13,22 @@ function menutoggle()   {
     }
 }
 
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
+}
+
+function login() {
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+}
+
 var ProductImg = document.getElementById("product-img");
 var SmallImg = document.getElementsByClassName("small-img");
 
@@ -35,3 +51,4 @@ SmallImg[3].onclick = function()
 {
     ProductImg.src = SmallImg[3].src;
 }
+
