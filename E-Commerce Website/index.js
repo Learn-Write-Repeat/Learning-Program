5e08840db25d44171933e08dabfc6c49f098f8ea
Md Adnan Hussain <hussainadnan906@gmail.com>
2021-08-05 15:29:32 +0530

    Modified pervious content and Added product-etails.html

--- E-Commerce Website/index.js
+++ E-Commerce Website/index.js
@@ -12,3 +12,26 @@ function menutoggle()   {
         MenuItems.style.maxHeight = "0px";
     }
 }
+
+var ProductImg = document.getElementById("product-img");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function()    
+{
+    ProductImg.src = SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function()    
+{
+    ProductImg.src = SmallImg[1].src;
+}
+
+SmallImg[2].onclick = function()    
+{
+    ProductImg.src = SmallImg[2].src;
+}
+
+SmallImg[3].onclick = function()    
+{
+    ProductImg.src = SmallImg[3].src;
+}
